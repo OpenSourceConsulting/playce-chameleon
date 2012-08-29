f37e357c334b303a473838a68d43bfdeb3f4cddf
nices96 <nices96@gmail.com>
2012-08-29 20:31:11 +0900

    Windows 실행환경 추가

---
+++ src/main/scripts/run.bat
@@ -0,0 +1,31 @@
+@CLS
+@ECHO OFF
+:: ======================================================================
+::                  Athena Chameleon Startup Script                      
+:: ======================================================================
+
+SET LOGGING_DIRECTORY=./log
+SET JAVA_OPTS="-Dlogging.directory=%LOGGING_DIRECTORY%"
+
+IF NOT DEFINED JAVA_HOME (
+    SET JAVA="%cd%\jre\bin\java"
+)
+
+:: Setup the Input Parameter(FQFN)
+SET FQFN=%1
+
+:: Display our environment
+echo "========================================================================="
+echo ""
+echo "  Athena Chameleon Bootstrap Environment"
+echo ""
+echo "  JAVA: %JAVA%"
+echo ""
+echo "  JAVA_OPTS: %JAVA_OPTS%"
+echo ""
+echo "  Parameter: %FQFN%"
+echo ""
+echo "========================================================================="
+echo ""
+
+%JAVA% %JAVA_OPTS% -jar athena-chameleon.jar %FQFN%
