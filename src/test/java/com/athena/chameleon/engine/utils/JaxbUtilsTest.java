f725a755e6580c4fb1b90b271779c6c68620403e
nices96 <nices96@gmail.com>
2012-09-06 22:22:49 +0900

    JaxbUtils를 이용한 marshal / unmarshal 샘플 추가

---
+++ src/test/java/com/athena/chameleon/engine/utils/JaxbUtilsTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision History
+ * Author			Date				Description
+ * ---------------	----------------	------------
+ * Sang-cheon Park	2012. 9. 6.		First Draft.
+ */
+package com.athena.chameleon.engine.utils;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.athena.chameleon.engine.entity.xml.jeusejbdd.v5_0.JeusEjbDdType;
+import com.athena.chameleon.engine.entity.xml.webapp.v2_5.WebAppType;
+import com.athena.chameleon.engine.entity.xml.weblogicejbjar.v9_0.WeblogicEjbJarType;
+
+/**
+ * <pre>
+ * 
+ * </pre>
+ * 
+ * @author Sang-cheon Park
+ * @version 1.0
+ */
+public class JaxbUtilsTest {
+    
+    @Test
+    public void webAppV25Test() {
+        String xml = this.getClass().getResource("/files/webapp/web.xml").getFile();
+        File file = new File(xml);
+        
+        try {
+            Object obj = JaxbUtils.unmarshal(WebAppType.class.getPackage().getName(), this.getClass().getResource("/files/webapp/").getFile(), file.getName());
+            
+            System.out.println(obj.getClass().getCanonicalName());
+            
+            String xmlData = JaxbUtils.marshal(WebAppType.class.getPackage().getName(), obj, new String[]{"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd"});
+            System.out.println(xmlData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error");
+        }
+    }
+
+    @Test
+    public void jeusEjbDdV50Test() {
+        String xml = this.getClass().getResource("/files/jeusejbdd/jeus-ejb-dd.xml").getFile();
+        File file = new File(xml);
+        
+        try {
+            Object obj = JaxbUtils.unmarshal(JeusEjbDdType.class.getPackage().getName(), this.getClass().getResource("/files/jeusejbdd/").getFile(), file.getName());
+            
+            System.out.println(obj.getClass().getCanonicalName());
+            
+            String xmlData = JaxbUtils.marshal(JeusEjbDdType.class.getPackage().getName(), obj, new String[]{"http://www.tmaxsoft.com/xml/ns/jeus schema/v5_0/jeus-ejb-dd.xsd"});
+            System.out.println(xmlData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error");
+        }
+    }
+
+    @Test
+    public void weblogicEjbJarV90Test() {
+        String xml = this.getClass().getResource("/files/weblogicejbjar/weblogic-ejb-jar.xml").getFile();
+        File file = new File(xml);
+        
+        try {
+            Object obj = JaxbUtils.unmarshal(WeblogicEjbJarType.class.getPackage().getName(), this.getClass().getResource("/files/weblogicejbjar/").getFile(), file.getName());
+            
+            System.out.println(obj.getClass().getCanonicalName());
+            
+            String xmlData = JaxbUtils.marshal(WeblogicEjbJarType.class.getPackage().getName(), obj, new String[]{"http://www.bea.com/ns/weblogic/90 http://www.bea.com/ns/weblogic/910/weblogic-ejb-jar.xsd"});
+            System.out.println(xmlData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error");
+        }
+    }
+
+}
+//end of JaxbUtilsTest.java
