9512add7f7327d865dda7ab22770c6e27d40dbc1
ienvyou <ienvyou@gmail.com>
2012-10-09 12:54:41 +0900

    ejb-jar 2.0 recreated

--- src/main/java/com/athena/chameleon/engine/entity/xml/ejbjar/v2_0/EnvEntryValue.java
+++ src/main/java/com/athena/chameleon/engine/entity/xml/ejbjar/v2_0/EnvEntryValue.java
@@ -1,8 +1,8 @@
-﻿//
+//
 // 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.2.6 버전을 통해 생성되었습니다. 
 // <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>를 참조하십시오. 
 // 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
-// 생성 날짜: 2012.09.05 시간 11:44:05 AM KST 
+// 생성 날짜: 2012.10.09 시간 12:53:10 PM KST 
 //
 
 
